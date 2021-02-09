397c72cc1f31b51dbcd745a3cd78e27748fea2f8
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2021-02-09 09:19:50 -0500

    Fixes

--- tarea1.cpp
+++ tarea1.cpp
@@ -60,16 +60,22 @@ int main() {
                 break;
             case 2:
                 cout << "Martes";
+                break;
             case 3:
                 cout << "Miercoles";
+                break;
             case 4:
                 cout << "Jueves";
+                break;
             case 5:
                 cout << "Viernes";
+                break;
             case 6:
                 cout << "Sabado";
+                break;
             case 7:
                 cout << "Domingo";
+                break;
             default:
                 break;
         }
