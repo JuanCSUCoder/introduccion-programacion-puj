d236ca6c8cc496aaa660df9b6647d988032a424a
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2021-02-24 18:15:44 -0500

    Excersie Summary

---
+++ tarea-sophia.cpp
@@ -0,0 +1,16 @@
+// https://sophia.javeriana.edu.co/programacion/sites/default/files/pdfParciales/2018-30-PensamientoAlgoritmico-1.pdf
+// Tennis Bot
+
+/**
+ * Instrucciones:
+ *  - Avanzar (a): Moverse hacia adelante 2 unidades
+ *  - Girar (g): Girar sobre su propio eje 90° a la izquierda
+ * 
+ * Orientaciónes:
+ *  - Jugador:
+ *      - Eje X + a la derecha
+ *      - Eje Y + al frente
+ *  - TennisBot:
+ *      - Eje X Positivo
+ *      - Ubicado en (0, 0)
+**/
