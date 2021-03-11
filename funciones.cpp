533f136c201d7c0386af93e21ca1a3b907d4cacf
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2021-03-11 09:30:13 -0500

    Add prime detection code using functions

--- funciones.cpp
+++ funciones.cpp
@@ -2,7 +2,34 @@
 
 using namespace std;
 
+bool esPrimo(int);
+
 int main() {
+    int num;
+
+    cout << "Ingrese un número: ";
+    cin >> num;
+
+    bool primo = esPrimo(num);
+
+    if(primo) {
+        cout << "El número es primo" << endl;
+    } else {
+        cout << "El número no es primo" << endl;
+    }
 
     return 0;
+}
+
+bool esPrimo(int n) {
+    bool resultado = true;
+    for (int i = 2; i < (n/2)+1; i++)
+    {
+        if (n%i==0)
+        {
+            resultado = false;
+        }
+    }
+
+    return resultado;
 }
