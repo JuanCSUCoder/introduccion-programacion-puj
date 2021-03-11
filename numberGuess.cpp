545bda0ce9fc7c2730f1f12bdc324bbffda778d2
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2021-03-11 10:03:06 -0500

    Format document

--- numberGuess.cpp
+++ numberGuess.cpp
@@ -1,29 +1,32 @@
 /* rand example: guess the number */
-#include <iostream>      /* printf, scanf, puts, NULL */
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
+#include <iostream> /* printf, scanf, puts, NULL */
+#include <stdlib.h> /* srand, rand */
+#include <time.h>   /* time */
 
 // Retrieved from: https://www.cplusplus.com/reference/cstdlib/rand/
 
 using namespace std;
 
-int main ()
+int main()
 {
   int iSecret, iGuess;
 
   /* initialize random seed: */
-  srand (time(NULL));
+  srand(time(NULL));
 
   /* generate secret number between 1 and 10: */
   iSecret = rand() % 10 + 1;
 
-  do {
+  do
+  {
     cout << "Guess the number (1 to 10): " << endl;
     cin >> iGuess;
-    if (iSecret<iGuess) cout << "The secret number is lower" << endl;
-    else if (iSecret>iGuess) cout << "The secret number is higher" << endl;
-  } while (iSecret!=iGuess);
+    if (iSecret < iGuess)
+      cout << "The secret number is lower" << endl;
+    else if (iSecret > iGuess)
+      cout << "The secret number is higher" << endl;
+  } while (iSecret != iGuess);
 
-  cout << "Congratulations!"<< endl;
+  cout << "Congratulations!" << endl;
   return 0;
 }
