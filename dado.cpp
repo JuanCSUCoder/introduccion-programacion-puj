1caa22595e7b5264d8cf520264686c98d73e822a
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2021-03-11 09:39:10 -0500

    Add dice simulator

---
+++ dado.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+int main() {
+    cout << "Inicializando con semilla aleatoria ..." << endl;
+    srand(time(NULL));
+
+    cout << "Simulando lanzamiento de un dado ...." << endl;
+    int resultado = rand() % 6 + 1;
+
+    cout << "En el dado salió: " << resultado << endl;
+
+    return 0;
+}
