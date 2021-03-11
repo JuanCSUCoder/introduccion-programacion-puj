480e930e2cf9774bf6e3867a3a6061684fe4bb25
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2021-03-11 09:48:47 -0500

    Add seed initialization

--- aleatorios.cpp
+++ aleatorios.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <time.h>
 
 // https://www.cplusplus.com/reference/cstdlib/rand
 
@@ -12,6 +13,9 @@ using namespace std;
 
 int main()
 {
+    cout << "Inicializando con semilla aleatoria ..." << endl;
+    srand(time(NULL));
+
     int v1 = rand() % 100;       // v1 in the range 0 to 99
     int v2 = rand() % 100 + 1;   // v2 in the range 1 to 100
     int v3 = rand() % 30 + 1985; // v3 in the range 1985-2014
