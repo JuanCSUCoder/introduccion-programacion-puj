b5a0da9ac2f021043e8941aa67c0d25b2e564240
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2021-03-11 09:24:54 -0500

    Add Notes

--- aleatorios.cpp
+++ aleatorios.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
+// https://www.cplusplus.com/reference/cstdlib/rand
+
+/**
+ * En los computadores no existe una forma de escojer algún número aleatorio, 
+ * por lo que se usna números pseudoaleatorio
+**/
+
 using namespace std;
 
 int main() {
     // Code
+    
 
     return 0;
 }
