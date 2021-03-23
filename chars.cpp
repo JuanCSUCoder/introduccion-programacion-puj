fdbbca45fe4e33a17f6d2ff8084d716f18dceba6
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2021-03-23 10:04:46 -0500

    Add Char vs String Comparation

--- chars.cpp
+++ chars.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
 int main() {
 	const char nombre[] = "Juan Camilo Sánchez Urrego";
 
+	cout << "Char[]: " << nombre << endl;
+
 	for (size_t i = 0; i < sizeof(nombre)/sizeof(nombre[0]); i++)
 	{
 		cout << nombre[i] << " --> " << (int)nombre[i] << endl;
-	}	
+	}
+
+	string nombre_str = "Juan Camilo Sánchez Urrego";
+
+	cout << "String: " << nombre_str << endl;
+
+	for (size_t i = 0; i < nombre_str.length(); i++)
+	{
+		cout << nombre_str[i] << " --> " << (int)nombre_str[i] << endl;
+	}
+	
+	return 0;
 }
