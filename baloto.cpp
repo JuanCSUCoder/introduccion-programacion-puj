3418b114e0c1e357eeb4eaf4f42ad7299972c1d2
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2021-04-06 16:18:32 -0500

    Finish Homework

--- baloto.cpp
+++ baloto.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <cstdlib> // rand();
+#include <time.h>	 // time();
 
 using namespace std;
 
-void inicializarArreglo(int vec[],int t, int valor){
- for (int i=0;i<t;i++){
-  vec[i]=valor;
- }
+void inicializarArreglo(int vec[], int t, int valor)
+{
+	for (int i = 0; i < t; i++)
+	{
+		vec[i] = valor;
+	}
 }
 
 void imprimirJuegoPropuesto(int vec[], int tama)
@@ -25,15 +29,50 @@ void imprimirJuegoPropuesto(int vec[], int tama)
 	}
 }
 
+bool existeEnElArreglo(int arr[], int tam, int a_buscar)
+{
+	bool existe = false;
+
+	for (size_t i = 0; i < tam; i++)
+	{
+		if (a_buscar == arr[i])
+		{
+			existe = true;
+		}
+	}
+
+	return existe;
+}
+
+void proponerJuego(int arr[], int tam, int desde, int hasta)
+{
+	srand(time(NULL));
+
+	for (size_t i = 0; i < tam; i++)
+	{
+		bool se_repite = true;
+		int candidato;
+
+		while (se_repite)
+		{
+			candidato = rand() % (hasta + 1) + desde;
+
+			se_repite = existeEnElArreglo(arr, tam, candidato);
+		}
+
+		arr[i] = candidato;
+	}
+}
+
 int main(){
-  const int T=100;
-  int arreglo[T];
-  int tam=6;
+	const int T=100;
+	int arreglo[T];
+	int tam=6;
 
 	inicializarArreglo(arreglo, tam, 0);
 	imprimirJuegoPropuesto(arreglo, tam);
-	// proponerJuego(arreglo,tam,1,43);
-	// imprimirJuegoPropuesto(arreglo,tam);
+	proponerJuego(arreglo,tam,1,43);
+	imprimirJuegoPropuesto(arreglo,tam);
 
 	return 0;
 }
