c1e4172a474f5e06f086b3bf1457471e35c68469
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2021-05-04 10:06:58 -0500

    Example Code

--- archivos.cpp
+++ archivos.cpp
@@ -2,8 +2,27 @@
 
 using namespace std;
 
+struct sPersona {
+	string nombre;
+	string apellido;
+
+	int edad;
+};
+
 int main()
 {
+	sPersona yo;
+
+	cout << "Nombres: ";
+	cin >> yo.nombre;
+
+	cout << "Apellidos: ";
+	cin >> yo.apellido;
+
+	cout << "Edad: ";
+	cin >> yo.edad;
+
+	cout << yo.apellido << '\t' << yo.nombre << '\t' << yo.edad << endl;
 
 	return 0;
 }
