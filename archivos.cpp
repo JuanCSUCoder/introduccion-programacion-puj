aca2a5961b6ffd57455315c579505e7fcb52b53a
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2021-05-04 10:24:05 -0500

    Add File Feature

--- archivos.cpp
+++ archivos.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-struct sPersona {
+struct sPersona
+{
 	string nombre;
 	string apellido;
 
@@ -13,16 +15,34 @@ int main()
 {
 	sPersona yo;
 
+	// Flujo del Archivo: este tiene la salida y entrada en uno solo
+	fstream archivo;
+
+	// // Flujo de Salida
+	// ofstream salida;
+
+	// // Flujo de Entrada
+	// ifstream entrada;
+
+	// Con ios::out Crea el archivo si no existe o lo reemplaza si ya existe
+	// Con ios::app Lo abre para añadirle algo al final y si no existe lo crea
+	archivo.open("datos/ejemplo_archivos.txt", ios::app);
+
+	if (archivo.fail())
+	{
+		cout << "Error al abrir el archivo" << endl;
+		return 1;
+	}
+
 	cout << "Nombres: ";
 	cin >> yo.nombre;
 
 	cout << "Apellidos: ";
 	cin >> yo.apellido;
-
 	cout << "Edad: ";
 	cin >> yo.edad;
 
-	cout << yo.apellido << '\t' << yo.nombre << '\t' << yo.edad << endl;
+	archivo << yo.apellido << '\t' << yo.nombre << '\t' << yo.edad << endl;
 
 	return 0;
 }
